317967fc9246c73b4a44e2f73f5e13f678f44fd5
nocturnixrepair802 <owner@nocturnixrepair.com>
2026-06-29 18:03:24 -0500

    Refactor CSS architecture and add design system

--- assets/js/router.js
+++ assets/js/router.js
@@ -4,25 +4,7 @@ Nocturnix Business Portal
 
 Router
 
-Version 0.5.0 Alpha
+Reserved for Future Development
+
 =========================================================
 */
-
-async function loadModule(moduleName) {
-  const workspace = document.getElementById("module-container");
-
-  try {
-    const response = await fetch(`modules/${moduleName}/index.html`);
-
-    const html = await response.text();
-
-    workspace.innerHTML = html;
-  } catch (error) {
-    workspace.innerHTML = `
-            <h1>Module Error</h1>
-            <p>Unable to load ${moduleName}</p>
-        `;
-
-    console.error(error);
-  }
-}
